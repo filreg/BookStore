580ebf36e29358683737a80f1f46de5db84d98d6
filippo reggimenti <reggimenti.filippo@gmail.com>
2020-12-03 12:47:04 +0100

    Created entities and BookStoreDbContext

---
+++ src/BookStore.Infrastructure/Mappings/CategoryMapping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace BookStore.Infrastructure.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Name).IsRequired().HasColumnType("varchar(150)");
+            builder.HasMany(c => c.Books)
+                .WithOne(b => b.Category)
+                .HasForeignKey(builder => builder.CategoryId);
+            builder.ToTable("Categories");
+           
+        }
+    }
+}
